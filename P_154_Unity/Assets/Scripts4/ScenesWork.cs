ee3d78dfe9cec47371caa947e7e5f0358db923fc
Mohand Karem Mohamed Mohany <mohand.karem@outlook.com>
2021-12-26 16:11:27 +0500

    Task IV Finished

---
+++ P_154_Unity/Assets/Scripts4/ScenesWork.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesWork : MonoBehaviour
+{
+  public int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetMouseButtonDown(0))      
+      {
+         SceneLoaderName();
+      }
+
+         if (Input.GetMouseButtonDown(1))      
+      {
+         SceneLoaderIndex();
+      }
+
+         if (Input.GetMouseButtonDown(2))      
+      {
+         SceneLoaderPublicIndex();
+      }
+
+    }    
+
+
+    void SceneLoaderName()
+     {
+       SceneManager.LoadScene("level2");
+       print("Loaded by NAME");
+     }
+    
+    void SceneLoaderIndex()
+     {
+       SceneManager.LoadScene(1);
+       print("Loaded by INDEX");
+     }
+
+    void SceneLoaderPublicIndex()
+     {
+       SceneManager.LoadScene(1);
+       print("Loaded by PUBLIC INDEX");
+     }
+
+}

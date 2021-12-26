31c7107eedc28513f9fea2e1a44761ff3c4b02a9
Mohand Karem Mohamed Mohany <mohand.karem@outlook.com>
2021-12-26 19:04:37 +0500

    Task V Finished

--- P_154_Unity/Assets/Scripts4/ScenesWork.cs
+++ P_154_Unity/Assets/Scripts4/ScenesWork.cs
@@ -15,7 +15,7 @@ public class ScenesWork : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      if (Input.GetMouseButtonDown(0))      
+        if (Input.GetMouseButtonDown(0))      
       {
          SceneLoaderName();
       }
